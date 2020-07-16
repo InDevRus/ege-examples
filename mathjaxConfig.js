d24ca48a022938abae4b2a5a865e0b926719df4f
InDev <InDevRus@hotmail.com>
2020-07-16 17:41:35 +0500

    Изменил отрисовку формул

--- mathjaxConfig.js
+++ mathjaxConfig.js
@@ -1,5 +1,5 @@
 window.MathJax = ({
     extensions: ["tex2jax.js"],
-    jax: ["input/TeX", "output/PreviewHTML"],
+    jax: ["input/TeX", "output/HTML-CSS"],
     tex2jax: {inlineMath: [['$', '$']], processEscapes: true},
 });
