56ed84d4b7310fc982af7c1e3e0e0319f9515841
InDevRus <InDevRus@hotmail.com>
2020-07-18 11:08:26 +0500

    Добавил исправления

--- mathjaxConfig.js
+++ mathjaxConfig.js
@@ -1,5 +1,5 @@
 window.MathJax = ({
     extensions: ["tex2jax.js"],
-    jax: ["input/TeX", "output/HTML-CSS"],
+    jax: ["input/TeX", "output/PreviewHTML"],
     tex2jax: {inlineMath: [['$', '$']], processEscapes: true},
 });
